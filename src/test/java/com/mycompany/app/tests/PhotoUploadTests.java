d4d8a366acf153934f4eea12da9c5c919ab413b9
inudzuka-mitsu <valeriiababina.qa@gmail.com>
2026-01-30 11:47:15 -0600

    removed unused page classes

--- src/test/java/com/mycompany/app/tests/PhotoUploadTests.java
+++ src/test/java/com/mycompany/app/tests/PhotoUploadTests.java
@@ -4,31 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import com.mycompany.app.base.TestBase;
 import com.mycompany.app.pages.CartPage;
-import com.mycompany.app.pages.CheckoutPage;
-import com.mycompany.app.pages.DesignToolPage;
-import com.mycompany.app.pages.EditorPage;
 import com.mycompany.app.pages.HomePage;
-import com.mycompany.app.pages.OrderConfirmationPage;
 import com.mycompany.app.pages.PhotoEditorPage;
 import com.mycompany.app.pages.ProductPage;
-import com.mycompany.app.pages.login.SignInPage;
 import com.mycompany.app.pages.login.StagingLoginPage;
-import com.mycompany.app.pages.modals_popups.AddressModal;
-import com.mycompany.app.pages.modals_popups.DesignPopup;
 
 public class PhotoUploadTests extends TestBase {
 
     private StagingLoginPage stagingLoginPage;
     private ProductPage productPage;
     private PhotoEditorPage photoEditorPage;
-    private DesignToolPage designToolPage;
-    private EditorPage editorPage;
-    private DesignPopup popup;
     private HomePage homePage;
-    private SignInPage signInPage;
-    private AddressModal shippingPage;
-    private CheckoutPage checkoutPage;
-    private OrderConfirmationPage confirmationPage;
     private CartPage cartPage;
 
     @Test
@@ -37,14 +23,7 @@ public class PhotoUploadTests extends TestBase {
         stagingLoginPage = new StagingLoginPage(page);
         productPage = new ProductPage(page);
         photoEditorPage = new PhotoEditorPage(page);
-        designToolPage = new DesignToolPage(page);
-        editorPage = new EditorPage(page);
-        popup = new DesignPopup(page);
         homePage = new HomePage(page);
-        signInPage = new SignInPage(page);
-        shippingPage = new AddressModal(page);
-        checkoutPage = new CheckoutPage(page);
-        confirmationPage = new OrderConfirmationPage(page);
 
         String PRODUCT_URL = getProperty("baseUrl") + "/Family-Photo-Personalized-Coffee-Mugs-p25561.prod?sdest=dept&sdestid=2115&storeid=34&categoryid=2115";
         String photoPath = System.getProperty("user.dir") + "/src/test/resources/lake.jpg";
