da6a6077d82faf5a68e9966c174fd221477fb12e
inudzuka-mitsu <valeriiababina.qa@gmail.com>
2026-03-27 10:34:41 -0500

    broken test fix

--- src/test/java/com/mycompany/app/tests/FileUploadTests.java
+++ src/test/java/com/mycompany/app/tests/FileUploadTests.java
@@ -47,47 +47,11 @@ public class FileUploadTests extends TestBase {
         photoGiftsPage.clickPillow();
 
         personalizeModal.clickAddToCart();
-        personalizeModal.clickContinue();
+        personalizeModal.clickContinueBtn();
 
         homePage.clickViewCart();
 
         cartPage.validateProductInCart("Your Own Photo Personalized 14\" Throw Pillow");
     }
-
-    // @Test
-    // void uploadPhotoVideo() {
-
-    //     stagingLoginPage = new StagingLoginPage(page);
-    //     productPage = new ProductPage(page);
-    //     photoUploadModal = new PhotoUploadModal(page);
-    //     personalizeModal = new PersonalizeItemModal(page);
-    //     cartPage = new CartPage(page);
-    //     homePage = new HomePage(page);
-
-    //     String PRODUCT_URL = getProperty("baseUrl") + "/Photo-Personalized-Keyring-with-Video-QR-Code-i164699.item?productid=59112&sdest=Search&sdestid=179337628";
-    //     String photoPath = System.getProperty("user.dir") + "/src/test/resources/jpegsystems-home.jpg";
-    //     String videoPath = System.getProperty("user.dir") + "/src/test/resources/flower_video.mp4";
-
-    //     page.navigate(getProperty("stagingBaseUrl"));
-    //     stagingLoginPage.closePopUp();
-
-    //     page.navigate(PRODUCT_URL);
-    //     productPage.clickPersonalizeBtn();
-
-    //     photoUploadModal.uploadPhoto(photoPath);
-    //     photoUploadModal.validatePhotoUploaded();
-
-    //     photoUploadModal.uploadVideo(videoPath);
-    //     //photoUploadModal.validateVideoUploaded();
-
-    //     // personalizeModal.clickAddToCart();
-
-    //     // homePage.clickViewCart();
-
-    //     // cartPage.validateProductInCart("Personalized Photo Keyring with Video QR Code");
-
-    //     page.pause();
-
-    // }
 
 }
